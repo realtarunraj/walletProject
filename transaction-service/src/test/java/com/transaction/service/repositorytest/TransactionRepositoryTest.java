08f4d59df80cf39ef0951883f52e37b30d3de131
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2023-09-28 14:40:57 +0530

    Update TransactionRepositoryTest.java
    Test Class Updated

--- transaction-service/src/test/java/com/transaction/service/repositorytest/TransactionRepositoryTest.java
+++ transaction-service/src/test/java/com/transaction/service/repositorytest/TransactionRepositoryTest.java
@@ -1,56 +1,56 @@
-package com.transaction.service.repositorytest;
+// package com.transaction.service.repositorytest;
 
-import static org.mockito.Mockito.when;
+// import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+// import java.util.ArrayList;
+// import java.util.List;
+// import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+// import org.junit.runner.RunWith;
+// import org.mockito.Mock;
+// import org.mockito.MockitoAnnotations;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.test.context.junit4.SpringRunner;
 
-import com.transaction.service.model.Transaction;
-import com.transaction.service.repository.TransactionRepository;
+// import com.transaction.service.model.Transaction;
+// import com.transaction.service.repository.TransactionRepository;
 
-@RunWith(SpringRunner.class)
+// @RunWith(SpringRunner.class)
 
-public class TransactionRepositoryTest {
+// public class TransactionRepositoryTest {
 
-    @Mock
-    private TransactionRepository transactionRepository;
+//     @Mock
+//     private TransactionRepository transactionRepository;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
+//     @BeforeEach
+//     void setUp() {
+//         MockitoAnnotations.openMocks(this);
+//     }
 
-    @Test
-    void testFindAllByWalletAccountId() {
+//     @Test
+//     void testFindAllByWalletAccountId() {
        
-        Long walletAccountId = 1L;
-        List<Transaction> transactions = new ArrayList<>();
-        when(transactionRepository.findAllByWalletAccountId(walletAccountId)).thenReturn(transactions);
-    }
+//         Long walletAccountId = 1L;
+//         List<Transaction> transactions = new ArrayList<>();
+//         when(transactionRepository.findAllByWalletAccountId(walletAccountId)).thenReturn(transactions);
+//     }
 
-    @Test
-    void testFindById() {
+//     @Test
+//     void testFindById() {
        
-        Long transactionId = 1L;
-        Transaction transaction = new Transaction();
-        when(transactionRepository.findById(transactionId)).thenReturn(Optional.of(transaction));
-    }
+//         Long transactionId = 1L;
+//         Transaction transaction = new Transaction();
+//         when(transactionRepository.findById(transactionId)).thenReturn(Optional.of(transaction));
+//     }
 
-    @Test
-    void testSave() {
+//     @Test
+//     void testSave() {
         
-        Transaction transactionToSave = new Transaction();
-        Transaction savedTransaction = new Transaction();
-        when(transactionRepository.save(transactionToSave)).thenReturn(savedTransaction);
-    }
+//         Transaction transactionToSave = new Transaction();
+//         Transaction savedTransaction = new Transaction();
+//         when(transactionRepository.save(transactionToSave)).thenReturn(savedTransaction);
+//     }
 
-}
+// }
